743f13e471d75d9271681289462619c1fb367066
mabrown8888 <69265058+mabrown8888@users.noreply.github.com>
2024-08-07 13:54:05 -0400

    d

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,10 +2,18 @@ import Header from '@/components/shared/Header'
 import React from 'react'
 import { transformationTypes } from '@/constants'
 import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = ({ params: { type } }:
+const AddTransformationTypePage = async ({ params: { type } }:
     SearchParamProps) => {
+    const { userId } = auth();
     const transformation = transformationTypes[type];
+
+    if (!userId) redirect('/sign-in')
+
+    const user = await getUserById(userId);
     return (
         <>
             <Header
@@ -13,7 +21,12 @@ const AddTransformationTypePage = ({ params: { type } }:
                 subtitle={transformation.subTitle}
             />
 
-            <TransformationForm />
+            <TransformationForm
+                action="Add"
+                userId={user._id}
+                type={transformation.type as TransformationTypeKey}
+                creditBalance={user.creditBalance}
+            />
         </>
     )
 }
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,17 +1,109 @@
 "use client" // because it has to manage keyboard and key press and submit events
 
-import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import { z } from "zod"
 
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+import { Button } from "@/components/ui/button"
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
+import { Input } from "@/components/ui/input"
+import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants"
+import { CustomField } from "./CustomField"
+import { AspectRatioKey } from "@/lib/utils"
+import { useState } from "react"
+
+export const formSchema = z.object({
+    title: z.string(),
+    aspectRation: z.string().optional(),
+    color: z.string().optional(),
+    prompt: z.string().optional(),
+    publicId: z.string(),
 })
 
-const TransformationForm = () => {
+const TransformationForm = ({ action, data = null, userId, type, creditBalance }: TransformationFormProps) => {
+    const transformationType = transformationTypes[type]; // keeping track of type of transformation
+    const [image, setImage] = useState(data) // keeping track of image
+    const [newTransformation, setNewTransformation] = useState<Transformations | null>(null); // keep track of new transf
+
+    const initialValues = data && action === 'Update' ? {
+        title: data?.title,
+        aspectRatio: data?.aspectRation,
+        color: data?.color,
+        prompt: data?.prompt,
+        publicId: data?.publicId,
+    } : defaultValues
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialValues,
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
+    const onSelectFieldHandler = (value: string, onChangeField: (value: string) => void) => {
+
+    }
+
     return (
-        <div>
-            Transformation Form
-        </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <CustomField
+                    control={form.control}
+                    name="title"
+                    formLabel="Image Title"
+                    className="w-full"
+                    render={({ field }) => <Input {...field}
+                        className="input-field" />}
+                />
+
+                {type === 'fill' && (
+                    <CustomField
+                        control={form.control}
+                        name="aspectRatio"
+                        formLabel="Aspect Ratio"
+                        className="w-full"
+                        render={({ field }) => (
+                            <Select
+                                onValueChange={(value) => onSelectFieldHandler(value, field.onChange)}
+                                value={field.value}
+                            >
+                                <SelectTrigger className="select-field">
+                                    <SelectValue placeholder="Select size" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {Object.keys(aspectRatioOptions).map((key) => (
+                                        <SelectItem key={key} value={key} className="select-item">
+                                            {aspectRatioOptions[key as AspectRatioKey].label}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        )}
+                    />
+                )}
+            </form>
+        </Form>
     )
 }
 

f00e369f07dfafe29203d9396dc3d55f3dccf4b6
mabrown8888 <69265058+mabrown8888@users.noreply.github.com>
2024-08-09 09:45:13 -0400

    d

--- middleware.ts
+++ middleware.ts
@@ -7,3 +7,5 @@ export default authMiddleware({
 export const config = {
     matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// In the context of the code you provided, using the authMiddleware from @clerk/nextjs with publicRoutes allows you to define specific routes in your Next.js application that can be accessed without requiring user authentication. This means that anyone, regardless of whether they are signed in or not, can access these routes.
